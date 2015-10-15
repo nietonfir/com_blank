643336908233fa3ef0975c3e0baaf925d09c8e94
nietonfir <nietonfir@gmail.com>
2015-10-16 00:09:35 +0200

    Refactor default view and layout

--- src/site/views/default/tmpl/default.php
+++ src/site/views/default/tmpl/default.php
@@ -10,17 +10,13 @@ defined('_JEXEC') or die;
  * @copyright   Copyright (C) 2015 nietonfir. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE
  */
-?>
-<!-- Blank Component 3.0.0 starts here -->
+
+if ($this->showContent == 1) : ?>
+<!-- main content for "<?php echo $this->title; ?>" -->
+<?php elseif ($this->showContent == 2) : ?>
 <div class="blank<?php echo $this->pageclass_sfx; ?>">
 <?php if ($this->params->get('show_page_heading', 1)) : ?>
-<h1>
-	<?php if ($this->escape($this->params->get('page_heading'))) :?>
-		<?php echo $this->escape($this->params->get('page_heading')); ?>
-	<?php else : ?>
-		<?php echo $this->escape($this->params->get('page_title')); ?>
-	<?php endif; ?>
-</h1>
+<h1><?php echo $this->escape($this->params->get('page_heading') ? $this->params->get('page_heading') : $this->params->get('page_title')); ?></h1>
 <?php endif; ?>
 </div>
-<!-- Blank Component 3.0.0 ends here -->
+<?php endif; ?>
--- src/site/views/default/view.html.php
+++ src/site/views/default/view.html.php
@@ -11,50 +11,54 @@ defined('_JEXEC') or die;
  * @license     GNU General Public License version 2 or later; see LICENSE
  */
 
-jimport('joomla.application.component.view');
-
 class BlankViewDefault extends JViewLegacy
 {
-    protected $params;
-
     public function display($tpl = null)
     {
         $app    = JFactory::getApplication();
         $params = $app->getParams();
-        $menus  = $app->getMenu();
-        $menu   = $menus->getActive();
+        $menu   = $app->getMenu()->getActive();
 
-        if ($menu)
-        {
-            $params->set('page_heading', $params->get('page_title', $menu->title));
+        // Check for errors.
+        if (count($errors = $this->get('Errors'))) {
+            JError::raiseWarning(500, implode("\n", $errors));
+
+            return false;
         }
-        else
-        {
+
+        if ($menu) {
+            $params->set('page_heading', $params->get('page_title', $menu->title));
+        } else {
             $params->set('page_title',  JText::_('Blank Component'));
         }
 
-        $title = $params->get('page_title');
-        if ($app->getCfg('sitename_pagetitles', 0)) {
-            $title = JText::sprintf('JPAGETITLE', $app->getCfg('sitename'), $title);
+        $title = $params->get('page_title', '');
+        if (empty($title)) {
+            $title = $app->get('sitename');
+        } else if ($app->get('sitename_pagetitles', 0) == 1) {
+            $title = JText::sprintf('JPAGETITLE', $app->get('sitename'), $title);
+        } else if ($app->get('sitename_pagetitles', 0) == 2) {
+            $title = JText::sprintf('JPAGETITLE', $title, $app->get('sitename'));
         }
         $this->document->setTitle($title);
 
-        if ($params->get('menu-meta_description'))
-        {
+        if ($params->get('menu-meta_description')) {
             $this->document->setDescription($params->get('menu-meta_description'));
         }
 
-        if ($params->get('menu-meta_keywords'))
-        {
+        if ($params->get('menu-meta_keywords')) {
             $this->document->setMetadata('keywords', $params->get('menu-meta_keywords'));
         }
 
-        if ($params->get('robots'))
-        {
+        if ($params->get('robots')) {
             $this->document->setMetadata('robots', $params->get('robots'));
         }
 
-        $this->assignRef('params',      $params);
+        // assign view variables
+        $this->showContent   = $params->get('content', 0);
+        $this->title         = $title;
+        $this->pageclass_sfx = htmlspecialchars($params->get('pageclass_sfx'));
+        $this->params        = $params;
 
         parent::display($tpl);
     }
